4f950ccd57d2243bc8067736f7ee1add29611a49
Rithikakalaimani <123987662+Rithikakalaimani@users.noreply.github.com>
2025-03-10 01:38:45 +0530

    updated contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -7,13 +7,13 @@ const Contact = () => {
       className='max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center'
     >
       <p className='font-titleFont text-lg text-textGreen font-semibold flex items-center tracking-wide'>
-        What's Next?
+        What&apos;s Next?
       </p>
       <h2 className='font-titleFont text-5xl font-semibold'>Get In Touch</h2>
       <p className='max-w-[600px] text-center text-textDark'>
-        I'm always open to new opportunities, collaborations, or just a tech
-        chat. If you have a question or a potential role that aligns with my
-        skills, feel free to reach out—I'd love to connect!
+        I&apos;m always open to new opportunities, collaborations, or just a
+        tech chat. If you have a question or a potential role that aligns with
+        my skills, feel free to reach out—I'd love to connect!
       </p>
 
       <a href='mailto:rithikakalaimani@gmail.com'>
